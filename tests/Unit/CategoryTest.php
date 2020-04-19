9606da4368d44e2ecda8420429d61b896f641e26
IAmatama <aimatama@gmail.com>
2020-04-19 17:56:28 -0300

    Correção do retorno da coluna ID

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+    
+}
